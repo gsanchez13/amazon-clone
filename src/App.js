cdc3e9819473edeebe0cf6194e4f9cf628a45892
gsanchez13 <gisellesanchez@pursuit.org>
2023-04-26 23:52:35 -0400

    inital commit for amazon-clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/checkout"
+            element={<h1>Checkout Page</h1>}>
+          </Route>
+          <Route
+            path="/login"
+            element={<h1>Login Page</h1>}>
+          </Route>
+          <Route
+            path="/"
+            element={<h1>Home Page!!</h1>}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className="header">
+        <img className="header__logo" 
+        src="/amazonlogo3.png"
+        
+        />
+        {/* logo on the left */}
+        {/* search box */}
+        {/* three links: 
+            sign in
+            returns & orders
+            your prime */}
+        {/* shopping cart / basket icon with a number*/}
+    </nav>
+  )
+}
+
+export default Header
