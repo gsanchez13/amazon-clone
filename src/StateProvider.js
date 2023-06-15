b68118442bb9822ee331b42ddaee96eda969a573
gsanchez13 <gisellesanchez@pursuit.org>
2023-06-14 23:59:07 -0400

    begins creating dataProvider

---
+++ src/StateProvider.js
@@ -0,0 +1,9 @@
+import { createContext, useContext, useReducer } from "react";
+
+//setting up data layer
+//for basket & user
+
+//Build provider and wrap app in entire provider
+
+//this is the data layer
+export const StateContext = createContext();
