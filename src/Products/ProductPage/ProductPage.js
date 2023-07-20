151f20b632cd5cbc650b1fc7600694bd9fe9705a
gsanchez13 <gisellesanchez@pursuit.org>
2023-07-19 23:51:48 -0400

    imports base for productpage

--- src/Products/ProductPage/ProductPage.js
+++ src/Products/ProductPage/ProductPage.js
@@ -1,1 +1,10 @@
 //individual page for product that allows add to cart state change functionality
+import React from 'react';
+function ProductPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default ProductPage;
