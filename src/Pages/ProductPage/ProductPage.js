987043bd2eae62819f92ed29beae3e9c659864c7
gsanchez13 <gisellesanchez@pursuit.org>
2023-07-25 23:48:13 -0400

    builds scaffold for productcard single product

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -1,13 +1,16 @@
 //individual page for product that allows add to cart state change functionality
 import React from 'react';
-import ProductCard from '../HomePage/ProductCard';
+import Product from '../HomePage/ProductCard';
 
-function ProductPage() {
+function ProductPage(className = 'productCard__single') {
     return (
         <div className="productPage__item">
             <img src="" alt="info" />
             <h3>Caption</h3>
-            <ProductCard />
+            <Product 
+            className="product__single"
+            image=""
+            title=""/>
         </div>
     )
 }
