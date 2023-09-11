34a4f1e59e90b542ae443e0035d4cc0cbeaa038f
gsanchez13 <gisellesanchez@pursuit.org>
2023-09-10 23:59:55 -0400

    dletes comments

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -4,7 +4,6 @@ import Product from '../HomePage/ProductCard';
 import './ProductPage.css';
 
 function ProductPage(className = 'productCard__single', id, title, description, reviews) {
-    //do a db call using id to obtain info to fill in for variables for this component. then, use variables from call to fill in page template
     return (
         <div className="productPage__item">
             <img src="" alt="info" />
